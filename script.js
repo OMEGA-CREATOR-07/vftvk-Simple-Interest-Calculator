4987062d2e783bc1f3235fa91d09e829c7f70519
OMEGA-CREATOR-07 <91074442+OMEGA-CREATOR-07@users.noreply.github.com>
2021-09-25 15:17:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,14 @@ function calculate(){
 var principal1=document.getElementById("amt").value;
 var years1=document.getElementById("year").value;
 var intamt=document.getElementById("interest").value;
+if(principal1==null||principal1==0){
+alert("please enter an positive number ");
+}
+else{
 var interests=principal1*(years1*(intamt/100));
 interests=interests.toFixed(2);
 var year=new Date().getFullYear()+parseInt(years1);
 document.getElementById("toprint").innerHTML= "If you deposit "+principal1+","+"<br>"+"at an interest rate of "+ intamt+"."+"<br>"+"You will receive an amount of "+interests+"<br>"+"in the year "+year+".";
 
-}
+}}
         
